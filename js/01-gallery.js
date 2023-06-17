6efe117027114200e76ea2600c136060e0ade9f0
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-06-17 19:50:13 +0300

    js7

---
+++ js/01-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+const galleryEl = document.querySelector('.gallery');
+const galleryAdd = galleryItems
+  .map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
+galleryEl.innerHTML = galleryAdd;
+const galleryLinks = document.querySelectorAll('.gallery__link');
+galleryEl.addEventListener('click', onGalleryClick);
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600"/>`);
+  instance.show();
+  window.addEventListener('keydown', onGalleryEsc);
+  function onGalleryEsc(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onGalleryEsc);
+    }
+  }
+}
